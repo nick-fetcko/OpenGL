730eaf647b3ed716689c7db3d0b8b218ed5e0cd4
Nick Fetcko <nick.fetcko@gmail.com>
2026-04-25 13:27:54 -0500

    Cube files are now loaded asynchronously

--- Cube.hpp
+++ Cube.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <filesystem>
+#include <thread>
 
 #include <glad/glad.h>
 
@@ -33,49 +34,67 @@ public:
 		Load(path);
 	}
 
-	void Load(const std::filesystem::path &path) const {
-		std::ifstream inFile(path, std::ios::in);
+	~Cube() {
+		if (loadThread.joinable())
+			loadThread.join();
+	}
 
-		std::string line;
-		std::size_t index = 0;
-		std::size_t tableDimensionSize = 0;
-		float *table = nullptr;
-		while (std::getline(inFile, line)) {
-			auto split = Utils::Split(line, std::isspace);
-			if (split.size() == 2 && split[0] == "LUT_3D_SIZE") {
-				tableDimensionSize = std::stoi(split[1]);
-				table = new float[static_cast<std::size_t>(std::pow(tableDimensionSize, 3) * 3)];
-			}
-			if (split.size() == 3 && split[0][0] != '#' && table) {
-				table[index * 3] = std::stof(split[0]);
-				table[index * 3 + 1] = std::stof(split[1]);
-				table[index * 3 + 2] = std::stof(split[2]);
+	void Load(const std::filesystem::path &path) {
+		this->path = path;
 
-				++index;
+		loadThread = std::thread([&] {
+			std::unique_lock lock(mutex);
+			std::ifstream inFile(this->path, std::ios::in);
+
+			std::string line;
+			std::size_t index = 0;
+			while (std::getline(inFile, line)) {
+				auto split = Utils::Split(line, std::isspace);
+				if (split.size() == 2 && split[0] == "LUT_3D_SIZE") {
+					tableDimensionSize = std::stoi(split[1]);
+					table = new float[static_cast<std::size_t>(std::pow(tableDimensionSize, 3) * 3)];
+				}
+				if (split.size() == 3 && split[0][0] != '#' && table) {
+					table[index * 3] = std::stof(split[0]);
+					table[index * 3 + 1] = std::stof(split[1]);
+					table[index * 3 + 2] = std::stof(split[2]);
+
+					++index;
+				}
 			}
+		});
+	}
+
+	void OnLoop() {
+		if (table && mutex.try_lock()) {
+			if (loadThread.joinable())
+				loadThread.join();
+
+			texture.Bind();
+
+			texture.TexImage3D(
+				tableDimensionSize,
+				tableDimensionSize,
+				tableDimensionSize,
+				table,
+				GL_FLOAT
+			);
+
+			glTexParameterf(GL_TEXTURE_3D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+			glTexParameterf(GL_TEXTURE_3D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+			glTexParameterf(GL_TEXTURE_3D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+			glTexParameterf(GL_TEXTURE_3D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+			glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+
+			texture.Unbind();
+
+			delete[] table;
+			table = nullptr;
+
+			LogDebug("Loaded .cube of size ", tableDimensionSize, " from ", path);
+
+			mutex.unlock();
 		}
-
-		texture.Bind();
-
-		texture.TexImage3D(
-			tableDimensionSize,
-			tableDimensionSize,
-			tableDimensionSize,
-			table,
-			GL_FLOAT
-		);
-
-		glTexParameterf(GL_TEXTURE_3D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-		glTexParameterf(GL_TEXTURE_3D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-		glTexParameterf(GL_TEXTURE_3D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-		glTexParameterf(GL_TEXTURE_3D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-		glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
-
-		texture.Unbind();
-
-		delete[] table;
-
-		LogDebug("Loaded .cube of size ", tableDimensionSize, " from ", path);
 	}
 
 	void Bind() const { texture.Bind(); }
@@ -83,5 +102,12 @@ public:
 
 private:
 	Texture3D texture;
+
+	std::thread loadThread;
+	std::mutex mutex;
+	std::size_t tableDimensionSize = 0;
+	float *table = nullptr;
+
+	std::filesystem::path path;
 };
 }
